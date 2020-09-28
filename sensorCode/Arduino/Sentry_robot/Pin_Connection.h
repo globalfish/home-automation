508ee6b5b04a6a8a85b157e64942140a94417312
Venkat Swaminathan <vswaminathan@dminc.com>
2020-09-28 00:36:37 -0400

    Create Pin_Connection.h

---
+++ sensorCode/Arduino/Sentry_robot/Pin_Connection.h
@@ -0,0 +1,22 @@
+#define MIC 2
+
+#define BUZZER 3
+
+#define PIR1 4
+
+// #define PAN 5     
+// #define TILT 6
+
+#define ULevel_ECHO 6
+#define U_TRIG 7
+#define U_ECHO 8
+
+#define M1_P 9
+#define M1_N 10
+#define M2_P 11
+#define M2_N 12
+
+#define FLASH 13  // WHITE LED TO FLASH
+
+#define LDR A0
+
